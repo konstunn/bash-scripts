258cb4b36e7a8f5e1c0771e012984fe3ccca3208
Konstantin Gorbunov <konstunn@ngs.ru>
2016-04-02 12:54:30 +0600

    Take code from alarm.sh, turn off daemon feature
    Handle command-line arguments.
    Detach daemon feature from text-menu interface.
    Reduce number of levels of text-menu from 2 to 1.

--- kickd.sh
+++ kickd.sh
@@ -1,11 +1,57 @@
 #!/bin/bash
 
-function dbg_echo {
-	if [ $DEBUG -eq 1 ] ; then
-		echo $1
-	fi
+function print_help {
+	echo -e "\nUsage:\n `basename $0` [options]"
+	echo -e "\nOptions:		\
+		\n --menu | -m		\
+		\n --process | -p	<process_name> \
+		\n --help | -h \n"
+		# TODO may be it would better to operate with pid 
+		# instead of process name ?
 }
 
+if [ $# -eq 0 ] ; then 
+	TEXT_MENU=1
+fi
+
+# parse command line arguments
+OPTS=+h,t:,m
+LONG_OPTS="help,menu,track:"
+
+ARGS=`getopt -o $OPTS --long $LONG_OPTS \
+     -n $(basename $0) -- "$@"`
+
+# if getopt returned error, claim and exit
+if [ $? -ne 0 ] ; then
+	echo "`basename $0`: specify '--help' or '-h' option for help." >&2
+	echo "`basename $0`: terminating..." >&2
+	exit 1
+fi
+
+eval set -- "$ARGS"
+
+while true ; do
+	case "$1" in 
+		--menu | -m)
+			TEXT_MENU=1 ; shift 
+		;;
+		--process | -p)
+			KICK_NOW=1;	PROCESS="$2" ; shift 2
+		;;
+		--help | -h)	print_help; exit 0 ;;
+		--)				shift ; break ;;
+	esac
+done
+
+# claim if extra arguments and exit
+if [ $# -gt 0 ] ; then
+	echo "`basename $0`: extra arguments \"$@\"" >&2
+	echo "`basename $0`: specify '--help' or '-h' option for help." >&2
+	echo "`basename $0`: terminating..." >&2
+	exit 1
+fi
+
+# search command "$1" in $PATH and in ./
 function find_command {
 	eval COMMAND=\$$1
 	VAR=`which $COMMAND`
@@ -24,6 +70,7 @@ function find_command {
 	return 0
 }
 
+# TODO adopt
 function track_n_kick {
 	while true ; do
 		pgrep `basename $1` > /dev/null
@@ -48,6 +95,7 @@ function track_n_kick {
 	done
 }
 
+# TODO adopt
 function ask_check_sleep_timeout {
 	read -p "Enter sleep timeout: " VAR
 	if [[ $VAR =~ ^[0-9]+[smhd]?$ ]] ; then
@@ -67,63 +115,43 @@ function ask_check_process_name {
 	fi
 }
 
-# TODO handle command-line arguments
-
-function print_set_daemon_menu {
-	echo "1. set process name ($PROCESS_NAME)" 
-	echo "2. set timeout ($SLEEP_TIMEOUT)"
-	echo "3. toggle debug messages ($DEBUG)"
-	echo "4. start daemon"
-	echo "5. exit"
-}
-
-PROCESS_NAME="./dummy.sh"
-DEBUG=1
-SLEEP_TIMEOUT=3
-
-function set_cron_jobs {
-	echo -n ""
-}
-
-JOB_HEADER=kickd.sh
-
 function list_cron_jobs {
-	# TODO make hamster readable
-	#crontab -l | sed -n "/^# $JOB_HEADER/{p;n;p}"
-	crontab -l | grep -A 1 -e "^# $JOB_HEADER"
-	if [ $? -eq 1 ] ; then
-		echo "No jobs."
+	# FIXME complete adoption from alarm.sh
+	crontab -l | grep -A 1 "^# kick.sh" \
+		| awk -F' |\t' \
+			'BEGIN { print "process name\tjob state\ttimeout"; i=0 }
+			/^# kick.sh/ { printf "%s\t",$3; i++ }
+			/^#?\t[0-9]/ { 
+				if ($1 == "#") printf "%s\t","off"
+				else printf "%s\t","on"
+				printf "%s:%s\t%s\t",$3,$2,$6;
+			}
+			END { if (i == 0) print "\nNo alarms." }'
+}
+
+# $1 - name, 
+# $2 - time (hh:mm), 
+# $3 - crontab day of week
+# $4 - path to track 
+function add_cron_job {
+	HOURS=$(echo $2 | awk -F':' '{print $1}')
+	MINUTES=$(echo $2 | awk -F':' '{print $2}')
+
+	DOW=$3 # crontab day of week 
+
+	TRACK="$4"
+
+	# TODO adopt
+	crontab -l \
+		| sed -e \ 
+		# TODO (crontab -l ; echo "" ) | crontab -
+		"\$a\# $JOB_HEADER $1\n
+		\t$MINUTES\t$HOURS\t\*\t\*\t$DOW\t$SELF_PATH -t \"$TRACK\"\n" \
+		| crontab -
+
+	if [ $? -eq 0 ] ; then
+		echo -e "\nJob was added"
 	fi
-}
-
-function print_set_cron_jobs_menu {
-	echo "1. list jobs"
-	echo "2. add job"
-	echo "3. delete job"
-	echo "4. set job"
-	echo "5. enable/disable job"
-	echo "6. exit"
-}
-
-function set_cron_jobs {
-	while true ; do
-		echo ""
-		print_set_cron_jobs_menu
-		echo ""
-		read -p "Enter your choice: " CHOICE
-		
-		echo ""
-		case "$CHOICE" in
-			1)	list_cron_jobs
-			;;
-			2)
-			;;
-			6)	return 0 ;;
-		esac
-		echo ""
-		read -p "Press Enter..."
-		clear
-	done
 }
 
 function set_daemon {
@@ -145,27 +173,48 @@ function set_daemon {
 	done
 }
 
-function print_main_menu {
-	echo "1. set daemon"
-	echo "2. set cron jobs"
-	echo "3. exit"
-} 
+if [ $TEXT_MENU -eq 1 ] ; then
 
-# main
-while true ; do
-	echo ""
-	print_main_menu	
-	echo ""
-	read -p "Enter your choice: " CHOICE
-	echo "" 
+	# if crontab does not exist
+	crontab -l > /dev/null		
+	if [ $? -gt 0 ] ; then
+		# create one
+		echo -e "\n" | crontab - 
+		# FIXME if crontab already exists
+		# and is absolutely empty "" then add job function fails 
+	else 
+		# backup crontab
+		echo -n ""
+		mkdir -p ./crontab.bkp
+		crontab -l > ./crontab.bkp/`date +%H%M%S-%d-%m-%Y`.crontab.bkp
+	fi
 
-	case "$CHOICE" in 
-		1)	set_daemon ;;
-		2)	set_cron_jobs ;;
-		3)	exit 0 ;;
-	esac
-	echo ""
-	read -p "Press Enter... "
-	clear
-done
+	while true ; do
+		echo ""
+		echo "1. list jobs"
+		echo "2. add job"
+		echo "3. delete job"
+		echo "4. set job"
+		echo "5. enable/disable job"
+		echo "6. exit"
+		echo ""
+		read -p "Enter your choice: " CHOICE
+		
+		echo ""
+		case "$CHOICE" in
+			1) list_cron_jobs
+			;;
+			2)
+			;;
+			6)	exit 0 ;;
+		esac
+		echo ""
+		read -p "Press Enter..."
+		clear
+	done
+fi
 
+if [ $KICK_NOW -eq 0 ] ; then exit 0; fi
+
+# kick now
+# TODO complete
